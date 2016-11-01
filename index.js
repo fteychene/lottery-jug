55e3cf96d2ddb29ea3cb36845a04fc20c1133e75
fteychene <francois.teychene@tabmo.io>
2016-11-01 19:05:30 +0100

    Change app for new specifications.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const rp = require('request-promise');
 
 const nbrite = new Nbrite({'token': process.env.EVENTBRITE_TOKEN});
+const event_id = process.env.EVENT_ID
 
 
 const range = (start, end) => {
@@ -22,8 +23,8 @@ const getAttendees = (event_id) => {
     )
 }
 
-const finWinnerWithTrustedRandom = (attendees, nbWinner) => {
-  return rp(`https://www.random.org/integers/?num=${nbWinner}&min=0&max=${attendees.length}&col=1&base=10&format=plain&rnd=new`)
+const findWinnerWithTrustedRandom = (attendees, nbWinner) => {
+  return (nbWinner >= attendees.length) ? attendees : rp(`https://www.random.org/integers/?num=${nbWinner}&min=0&max=${attendees.length}&col=1&base=10&format=plain&rnd=new`)
       .then(function (body) {
        console.log(body);
         return body.split("\n")
@@ -31,17 +32,25 @@ const finWinnerWithTrustedRandom = (attendees, nbWinner) => {
                   .map(Number)
                   .map((index)=> { return attendees[index]})
       })
-}
+    }
 
 
 const app = express();
 
 
-app.get('/:eventId/:nbWinners', function(req, res){
-  getAttendees(req.params.eventId)
-    .then(attendees => finWinnerWithTrustedRandom(attendees, req.params.nbWinners))
-    .then(winners => winners.map(({profile: {first_name: first_name, last_name: last_name}}) => { return {'Winner': `${first_name} ${last_name}`}}))
-    .then(winners => res.send(winners))
+app.get('/winners', function(req, res){
+  const nb_winner = req.query.nb
+  if (nb_winner == undefined || isNaN(nb_winner) || nb_winner < 0) {
+    res.status(400)
+    res.json({error: 'Nb param should be a positive integer'})
+  } else if (nb_winner == 0) {
+    res.json([])
+  } else {
+    getAttendees(event_id)
+      .then(attendees => findWinnerWithTrustedRandom(attendees, nb_winner))
+      .then(winners => winners.map(({profile: {first_name: first_name, last_name: last_name}}) => { return {'Winner': `${first_name} ${last_name}`}}))
+      .then(winners => res.json(winners))
+    }
 });
 
 app.listen(3000);
