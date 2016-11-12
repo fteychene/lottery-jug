d0c80ca97027806585cf67d6b0095fb35f421ce5
fteychene <francois.teychene@tabmo.io>
2016-11-12 17:07:22 +0100

    Update the random generator. Change fetching of eventbrite for standard fetch. Correction of various bug.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const Nbrite = require('nbrite');
 const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
 const rp = require('request-promise');
 
-const nbrite = new Nbrite({'token': process.env.EVENTBRITE_TOKEN});
-const event_id = process.env.EVENT_ID
+const token = process.env.EVENTBRITE_TOKEN;
+const organizer = process.env.ORGANIZER_TOKEN;
 
+console.log(`Starting lottery for ${organizer} with token ${token}`);
+
+const nbrite = new Nbrite({'token': token});
 
 const range = (start, end) => {
   let pages = []
@@ -14,29 +19,40 @@ const range = (start, end) => {
   return pages
 }
 
+const get = (path) => fetch(path).then(payload => payload.json())
+
+const getRandomIds = (max, numbers) => {
+  console.log(`Generating ${numbers} random numbers for ${max}`)
+  let results = range(0, numbers - 1).reduce((acc, _) => {
+      let index
+      do {
+        index =  Math.floor(Math.random() * max)
+      } while (acc.includes(index));
+      acc.push(index)
+      return acc
+    }, [])
+    return results
+  }
+
 const getAttendees = (event_id) => {
-    return nbrite.get(`/events/${event_id}/attendees`).then(attendees =>
+    return get(`https://www.eventbriteapi.com/v3/events/${event_id}/attendees/?token=${token}`)
+      .then(attendees =>
       Promise.all(
         range(attendees.pagination.page_number +1, attendees.pagination.page_count)
-          .reduce((acc, val) => acc.concat([nbrite.get(`/events/${event_id}/attendees`, { 'page': val }).then(data => data.attendees)]), attendees.attendees))
+          .reduce((acc, val) => acc.concat([get(`https://www.eventbriteapi.com/v3/events/${event_id}/attendees?token=${token}&page=${val}`).then(data => data.attendees)]), attendees.attendees))
         .then(values => [].concat.apply([], values))
     )
 }
 
-const findWinnerWithTrustedRandom = (attendees, nbWinner) => {
-  return (nbWinner >= attendees.length) ? attendees : rp(`https://www.random.org/integers/?num=${nbWinner}&min=0&max=${attendees.length}&col=1&base=10&format=plain&rnd=new`)
-      .then(function (body) {
-       console.log(body);
-        return body.split("\n")
-                  .filter((element)=>{return element.length > 0})
-                  .map(Number)
-                  .map((index)=> { return attendees[index]})
-      })
-    }
-
+const errorRequest = (res, status, error) => {
+  console.error(error)
+  res.status(status);
+  res.json(error);
+}
 
 const app = express();
 
+app.use(cors());
 
 app.get('/winners', function(req, res){
   const nb_winner = req.query.nb
@@ -46,11 +62,21 @@ app.get('/winners', function(req, res){
   } else if (nb_winner == 0) {
     res.json([])
   } else {
-    getAttendees(event_id)
-      .then(attendees => findWinnerWithTrustedRandom(attendees, nb_winner))
+    console.log(`Generating ${nb_winner} winners for event`)
+    get(`https://www.eventbriteapi.com/v3/events/search/?sort_by=date&organizer.id=${organizer}&token=${token}`)
+      .then(data => {
+        if (data.events.length < 1) {
+          return Promise.reject(new Error("No event available"))
+        }
+        return data.events[0].id
+      })
+      .then(event_id => getAttendees(event_id))
+      .then(attendees => getRandomIds(attendees.length, 4).map((index) => attendees[index]))
+      .then(winners => {console.log("Found winners"); return winners})
       .then(winners => winners.map(({profile: {first_name: first_name, last_name: last_name}}) => { return {'Winner': `${first_name} ${last_name}`}}))
       .then(winners => res.json(winners))
-    }
+      .catch(error => errorRequest(res, 500, error))
+  }
 });
 
 app.listen(3000);
